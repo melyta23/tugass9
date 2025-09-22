6decf1474b9cd14d6ca6fc19a4ae6ab348fde5b2
Mely Tri Andini <melytriandini@gmail.com>
2025-09-22 16:47:56 +0700

    koneksi 1

--- koneksi.php
+++ koneksi.php
@@ -1,27 +1,12 @@
 <?php
-include "koneksi.php";
+$host = "localhost";
+$user = "root";   // atau username MySQL di server sekolah
+$pass = "";       // kalau server pakai password, isi di sini
+$db   = "db_xirpl1-13_1";
 
-// Query ambil data
-$sql = "SELECT * FROM bioskop";
-$result = mysqli_query($koneksi, $sql);
+$koneksi = mysqli_connect($host, $user, $pass, $db);
 
-if (mysqli_num_rows($result) > 0) {
-    echo "<table border='1' cellpadding='8'>";
-    echo "<tr><th>ID</th><th>Film</th><th>Jadwal</th><th>Penonton</th><th>Tiket</th></tr>";
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>".$row['id']."</td>";
-        echo "<td>".$row['film']."</td>";
-        echo "<td>".$row['jadwal']."</td>";
-        echo "<td>".$row['penonton']."</td>";
-        echo "<td>".$row['tiket']."</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "Tidak ada data.";
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-mysqli_close($koneksi);
 ?>
