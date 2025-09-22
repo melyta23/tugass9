5e2a9d6cd4b38cb970361793501bb9c4e4aa5b3a
Mely Tri Andini <melytriandini@gmail.com>
2025-09-22 16:51:19 +0700

    komitan 1

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";   // atau username MySQL di server sekolah
-$pass = "";       // kalau server pakai password, isi di sini
-$db   = "db_xirpl1-13_1";
+// Tampilkan semua error biar jelas
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+$host = "localhost";   // atau bisa coba "127.0.0.1"
+$user = "root";        // username MySQL (cek lagi di server sekolah, bisa jadi bukan root)
+$pass = "";            // isi password kalau ada
+$db   = "db_xirpl1-13_1"; // nama database
+
+// Membuat koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
+// Cek koneksi
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi berhasil!";
 }
 ?>
